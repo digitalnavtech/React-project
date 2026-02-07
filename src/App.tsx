666bd5d407a6fd046e34c632e248bf5a0b9273d6
Navdeep <digitalnavtech6@gmail.com>
2026-02-07 19:46:35 +0200

    Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Navdeep Singh Full Stack Finland 8000 </h1>
+      <h1>Navdeep Singh Full Stack Finland 8000 Now </h1>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
